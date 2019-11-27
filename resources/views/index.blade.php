805e50dc65532766f8a26c2c467bfc532f183dba
Daniel Duris <dusoft@gmail.com>
2019-11-27 21:19:37 +0100

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -65,7 +65,7 @@
         chart: {
           title: 'Kvalita vzduchu po dňoch'
         },
-        width: 900,
+        width: 1200,
         height: 500,
         series: {
           // Gives each series an axis name that matches the Y-axis below.
@@ -79,33 +79,6 @@
         }
       };
 
-      var classicOptions = {
-        title: 'Average Temperatures and Daylight in Iceland Throughout the Year',
-        width: 900,
-        height: 500,
-        // Gives each series an axis that matches the vAxes number below.
-        series: {
-          0: {targetAxisIndex: 0},
-          1: {targetAxisIndex: 1}
-        },
-        vAxes: {
-          // Adds titles to each axis.
-          0: {title: 'Temps (Celsius)'},
-          1: {title: 'Daylight'}
-        },
-        hAxis: {
-          ticks: [new Date(2014, 0), new Date(2014, 1), new Date(2014, 2), new Date(2014, 3),
-                  new Date(2014, 4),  new Date(2014, 5), new Date(2014, 6), new Date(2014, 7),
-                  new Date(2014, 8), new Date(2014, 9), new Date(2014, 10), new Date(2014, 11)
-                 ]
-        },
-        vAxis: {
-          viewWindow: {
-            max: 30
-          }
-        }
-      };
-
       function drawMaterialChart() {
         var materialChart = new google.charts.Line(chartDiv);
         materialChart.draw(data, materialOptions);
@@ -113,16 +86,18 @@
         button.onclick = drawClassicChart;
       }
 
-      function drawClassicChart() {
-        var classicChart = new google.visualization.LineChart(chartDiv);
-        classicChart.draw(data, classicOptions);
-        button.innerText = 'Change to Material';
-        button.onclick = drawMaterialChart;
-      }
-
       drawMaterialChart();
 
     }
     </script>
+    <style>
+        .chart {
+          width: 100%;
+          min-height: 450px;
+        }
+        .row {
+          margin:0 !important;
+        }
+    </style>
   </body>
 </html>
